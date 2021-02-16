58f46fc3419f2004da3f046dbede1ad6aa92a0b7
FilipLeonard <leonard.filip.93@gmail.com>
2021-02-16 12:59:57 +0200

    added admin logic to get player games

--- public/js/admin-app.js
+++ public/js/admin-app.js
@@ -1,7 +1,11 @@
 const dynastySelect = document.getElementById('dynasty-select');
 const playersList = document.getElementById('players-list');
+const gamesSection = document.getElementById('games');
+const gameStateSelect = document.getElementById('game-status-select');
+const playerGamesList = document.getElementById('player-games-list');
 
 dynastySelect.addEventListener('input', event => {
+  clearGameList();
   const selectedDynasty = dynastySelect.value;
 
   const players = playersList.children;
@@ -24,6 +28,36 @@ playersList.addEventListener('click', async e => {
   const res = await fetch(`/potato/games/${name}`);
 
   if (!res.ok) return console.log('fetch games failed');
-  const games = await res.json();
-  console.log(games);
+  const { data } = await res.json();
+  const { games } = data;
+
+  const markup = `${games
+    .map(
+      g =>
+        `<li data-state=${g.state}>${g.state} | ${g.whacks} | ${g.partialPoints}</li>`
+    )
+    .join('')}`;
+  clearGameList();
+  playerGamesList.insertAdjacentHTML('afterbegin', markup);
+  gamesSection.classList.remove('hidden');
 });
+
+gameStateSelect.addEventListener('input', event => {
+  const selectedState = gameStateSelect.value;
+
+  const games = playerGamesList.children;
+  for (const game of games) {
+    console.log({ selectedState, currentState: game.dataset.state });
+    if (selectedState === 'all') {
+      game.classList.remove('hidden');
+    } else if (selectedState === game.dataset.state) {
+      game.classList.remove('hidden');
+    } else {
+      game.classList.add('hidden');
+    }
+  }
+});
+
+function clearGameList() {
+  playerGamesList.innerHTML = '';
+}
